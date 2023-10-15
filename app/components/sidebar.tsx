5703ac2ee9a480e4b3ae225aff041e166af72f5f
taotao033 <tao.zhang.nlp@gmail.com>
2023-10-15 18:46:51 +0800

    修改title提示

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -145,11 +145,12 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          授权密码请联系管理员。
         </div>
+        <div className={styles["sidebar-sub-title"]}>微信：DeepLearning996</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
